4f8c6535c63223491017bc5e462daee7535ae63c
EmilianaManzo <emiliana.ma96@gmail.com>
2024-03-19 15:51:41 +0100

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,37 @@
+const { createApp } = Vue;
+
+
+
+createApp({
+  data(){
+    return {
+      apiUrl : 'https://flynn.boolean.careers/exercises/api/random/mail',
+      emails : [],
+      
+    }
+  },
+
+  methods : {
+    getApi(){
+      for(let i= 0; i < 10; i++){
+        axios.get(this.apiUrl)
+              .then( (res) => {
+                console.log('Dati');
+                console.log(res.data);
+                console.log(res.data.response);
+                this.emails.push(res.data.response)
+              })
+              .catch( (err) => {
+                console.log(err);
+              })
+      }
+
+      }
+  },
+
+  created(){
+    this.getApi()
+    console.log(this.emails);
+  }
+  
+}).mount('#app')
